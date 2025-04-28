094217bc18afafe8e4eecdc7c34fca67d2843ff4
mtalala <superraioi2014@gmail.com>
2025-04-28 18:17:01 -0300

    feat: update services and create controllers with REST endpoints; remove ConsoleApp

--- src/main/java/br/milliecarol/projeto/ProjetoApplication.java
+++ src/main/java/br/milliecarol/projeto/ProjetoApplication.java
@@ -1,26 +1,12 @@
 package br.milliecarol.projeto;
 
-import br.milliecarol.projeto.aplicacao.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoApplication implements CommandLineRunner {
+public class ProjetoApplication {
 
-	@Autowired
-	ConsoleApp consoleApp;
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoApplication.class, args);
-	}
-
-	@Override
-	public void run(String...Args) throws Exception {
-		ConsoleApp.iniciar();
-	}
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoApplication.class, args);
+    }
 }
